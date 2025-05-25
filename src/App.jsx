b804f6a40f5ed6ddda514ba17e00aa83f39ec0b5
Saodat <saodat.riskiyeva@gmail.com>
2025-05-25 19:59:51 -0400

    creating routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <div>
+      <h1>Hello Router!</h1>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
