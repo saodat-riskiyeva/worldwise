e11b55300359261347a3c99d5e9da333961f0a89
Saodat <saodat.riskiyeva@gmail.com>
2025-05-26 14:33:49 -0400

    nested routes and index route

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>List of cities</p>} />
             <Route path="cities" element={<p>List of cities</p>} />
             <Route path="countries" element={<p>Countries</p>} />
             <Route path="form" element={<p>Form</p>} />
