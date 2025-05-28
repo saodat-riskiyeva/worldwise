3811a350d63e75372d82412f8177ece099d1c5bd
Saodat <saodat.riskiyeva@gmail.com>
2025-05-27 23:58:45 -0400

    implementing the countries list

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,29 @@
+import Spinner from "./Spinner";
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first country by clicking on a country n the map" />
+    );
+
+  const countries = cities.reduce((arr, element) => {
+    if (!arr.map((item) => item.country).includes(element.country))
+      return [...arr, { country: element.country, emoji: element.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
