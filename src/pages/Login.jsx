84885e30a14e3b2d09f4bcca25f4b61ac4c8343f
Saodat <saodat.riskiyeva@gmail.com>
2025-06-03 22:53:57 -0400

    challenge 1-3 questions

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (isAuthenticated === true) navigate("/app");
+    },
+    [isAuthenticated, navigate]
+  );
 
   return (
     <main className={styles.login}>
